cfcebd1f7e2e915308f3746331ffbee7d4706c92
ofwebdev <nkomorfaruk@gmail.com>
2023-09-29 01:59:11 +0600

    Add cloudinary to show img on client side

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "../../libs/prismadb";
 
 
-export function GET(request: NextRequest) {
-    return NextResponse.json([
-        { id: 1, name: 'Milk', price: 2.5 },
-        { id: 2, name: 'Bread', price: 2.5 }
-    ])
+
+export async function GET(request: NextRequest) {
+    const product = await prisma.product.findMany()
+    return NextResponse.json(product)
 }
 
 
@@ -14,7 +14,14 @@ export async function POST(request: NextRequest) {
     const body = await request.json();
     const validation = schema.safeParse(body);
 
+
     if (!validation.success)
         return NextResponse.json(validation.error.errors, { status: 400 })
-    return NextResponse.json({ id: 10, name: body.name, price: body.price }, { status: 201 })
+    const newProduct = await prisma.product.create({
+        data: {
+            name: body.name,
+            price: body.price
+        }
+    })
+    return NextResponse.json(newProduct, { status: 201 })
 }
---
+++ app/upload/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useState } from "react";
+
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+  source_url: string;
+  url: string;
+}
+
+export default function UploadPage() {
+  const [publicId, setPublicId] = useState("");
+  // const [imgSource, setImgSource] = useState("");
+  console.log(publicId);
+  return (
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={700}
+          height={500}
+          alt={"fistImg"}
+        ></CldImage>
+      )}
+      <CldUploadWidget
+        uploadPreset={"onluyc1s"}
+        onUpload={(result, widgets) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult; // Define for type recolonization
+          setPublicId(info.public_id);
+          // console.log(result);
+        }}
+      >
+        {({ open }) => (
+          <button onClick={() => open()} className="btn btn-primary">
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
+  );
+}
